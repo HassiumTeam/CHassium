1ff4246a13660f3be99755045d1fb208ef1d5fbb
Jacob Misirian <misirianrule@gmail.com>
2019-08-17 12:29:17 -0500

    Added parsing function declarations

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -14,6 +14,57 @@ void parser_free (struct parser_state * state) {
     free (state);
 }
 
+// ----------------------------------------------------------------------------
+// Functions for parsing statements.
+// ----------------------------------------------------------------------------
+static struct ast_node * parse_stmt          (struct parser_state * state);
+static struct ast_node * parse_block         (struct parser_state * state);
+static struct ast_node * parse_break         (struct parser_state * state);
+static struct ast_node * parse_class         (struct parser_state * state);
+static struct ast_node * parse_continue      (struct parser_state * state);
+static struct ast_node * parse_for           (struct parser_state * state);
+static struct ast_node * parse_foreach       (struct parser_state * state);
+static struct ast_node * parse_func          (struct parser_state * state);
+static struct ast_node * parse_if            (struct parser_state * state);
+static struct ast_node * parse_import        (struct parser_state * state);
+static struct ast_node * parse_raise         (struct parser_state * state);
+static struct ast_node * parse_return        (struct parser_state * state);
+static struct ast_node * parse_super         (struct parser_state * state);
+static struct ast_node * parse_try_catch     (struct parser_state * state);
+static struct ast_node * parse_use           (struct parser_state * state);
+static struct ast_node * parse_while         (struct parser_state * state);
+
+// ----------------------------------------------------------------------------
+// Miscellaneous parsing functions.
+// ----------------------------------------------------------------------------
+static struct vector_state * parse_access_chain  (struct parser_state * state);
+static struct vector_state * parse_arg_list      (struct parser_state * state);
+static struct vector_state * parse_func_args     (struct parser_state * state);
+static struct vector_state * parse_object_params (struct parser_state * state);
+
+// ----------------------------------------------------------------------------
+// Function for parsing expressions.
+// ----------------------------------------------------------------------------
+static struct ast_node * parse_expr_stmt  (struct parser_state * state);
+static struct ast_node * parse_expr       (struct parser_state * state);
+static struct ast_node * parse_assign     (struct parser_state * state);
+static struct ast_node * parse_or         (struct parser_state * state);
+static struct ast_node * parse_and        (struct parser_state * state);
+static struct ast_node * parse_eq         (struct parser_state * state);
+static struct ast_node * parse_instanceof (struct parser_state * state);
+static struct ast_node * parse_comp       (struct parser_state * state);
+static struct ast_node * parse_add        (struct parser_state * state);
+static struct ast_node * parse_mult       (struct parser_state * state);
+static struct ast_node * parse_unary      (struct parser_state * state);
+static struct ast_node * parse_access     (struct parser_state * state);
+static struct ast_node * parse_term       (struct parser_state * state);
+static struct ast_node * parse_closure    (struct parser_state * state);
+static struct ast_node * parse_obj_decl   (struct parser_state * state);
+static struct ast_node * parse_typeof     (struct parser_state * state);
+
+// ----------------------------------------------------------------------------
+// Functions for navigating the tokens.
+// ----------------------------------------------------------------------------
 static       int    at_eof     (struct parser_state * state);
 static       int    match      (struct parser_state * state, tok_type_t type);
 static       int    matchv     (struct parser_state * state, tok_type_t type, const char * val);
@@ -22,7 +73,6 @@ static       int    acceptv    (struct parser_state * state, tok_type_t type, co
 static       char * expect     (struct parser_state * state, tok_type_t type);
 static       char * expectv    (struct parser_state * state, tok_type_t type, const char * val);
 
-static struct ast_node * parse_stmt (struct parser_state * state);
 
 struct ast_node * parse (struct parser_state * state) {
     struct vector_state * stmts;
