7cbbd3cf5b96e5ff31b1be8231a6a920817514dc
Jacob Misirian <misirianrule@gmail.com>
2019-09-03 10:50:07 -0500

    Work on number

--- src/has_lib/modules/default/number.c
+++ src/has_lib/modules/default/number.c
@@ -1,9 +1,16 @@
 #include <has_lib/modules/default/number.h>
 
-static struct has_obj * _add      (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
-static struct has_obj * _equal    (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
-static struct has_obj * to_number (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
-static struct has_obj * to_string (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
+static struct has_obj * _add               (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
+static struct has_obj * _divide            (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
+static struct has_obj * _equal             (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
+static struct has_obj * _greater           (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
+static struct has_obj * _greater_or_equal  (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
+static struct has_obj * _lesser            (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
+static struct has_obj * _lesser_or_equal   (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
+static struct has_obj * _logical_and       (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
+static struct has_obj * _logical_or        (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
+static struct has_obj * to_number          (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
+static struct has_obj * to_string          (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
 
 struct has_obj * has_number_init (float f) {
     struct has_number * state;
@@ -13,10 +20,17 @@ struct has_obj * has_number_init (float f) {
     state->val = f;
 
     obj = has_obj_init (get_number_type (), state, has_number_free);
-    has_obj_set_attrib (obj, "_add",     has_method_init (obj, _add,      NULL));
-    has_obj_set_attrib (obj, "_equal",   has_method_init (obj, _equal,    NULL));
-    has_obj_set_attrib (obj, "toNumber", has_method_init (obj, to_number, NULL));
-    has_obj_set_attrib (obj, "toString", has_method_init (obj, to_string, NULL));
+    has_obj_set_attrib (obj, "_add",              has_method_init (obj, _add,         NULL));
+    has_obj_set_attrib (obj, "_divide",           has_method_init (obj, _divide,      NULL));
+    has_obj_set_attrib (obj, "_equal",            has_method_init (obj, _equal,       NULL));
+    has_obj_set_attrib (obj, "_greater",          has_method_init (obj, _greater,     NULL));
+    has_obj_set_attrib (obj, "_greater_or_equal", has_method_init (obj, _greater,     NULL));
+    has_obj_set_attrib (obj, "_lesser",           has_method_init (obj, _greater,     NULL));
+    has_obj_set_attrib (obj, "_lesser_or_equal",  has_method_init (obj, _greater,     NULL));
+    has_obj_set_attrib (obj, "_logical_and",      has_method_init (obj, _logical_and, NULL));
+    has_obj_set_attrib (obj, "_logical_or",       has_method_init (obj, _logical_or,  NULL));
+    has_obj_set_attrib (obj, "toNumber",          has_method_init (obj, to_number,    NULL));
+    has_obj_set_attrib (obj, "toString",          has_method_init (obj, to_string,    NULL));
 
     return obj;
 }
@@ -39,10 +53,20 @@ static struct has_obj * _add (struct vm_state * vm, struct has_obj * self, struc
     struct has_number * num;
     struct has_number * right;
 
-    num   = (struct has_number *)self                                    ->state;
+    num   = (struct has_number *)self->state;
     right = (struct has_number *)((struct has_obj *)vector_get (args, 0))->state;
 
     return has_number_init (num->val + right->val);
+}
+
+static struct has_obj * _divide (struct vm_state * vm, struct has_obj * self, struct vector_state * args) {
+    struct has_number * this;
+    struct has_number * right;
+
+    this  = (struct has_number *)self->state;
+    right = (struct has_number *)((struct has_obj *)vector_get (args, 0))->state;
+
+    return has_number_init (this->val / right->val);
 }
 
 static struct has_obj * _equal (struct vm_state * vm, struct has_obj * self, struct vector_state * args) {
@@ -55,7 +79,87 @@ static struct has_obj * _equal (struct vm_state * vm, struct has_obj * self, str
     if (this->val == right->val) {
         return HAS_TRUE;
     }
-    
+
+    return HAS_FALSE;
+}
+
+static struct has_obj * _greater (struct vm_state * vm, struct has_obj * self, struct vector_state * args) {
+    struct has_number * this;
+    struct has_number * arg;
+
+    this = (struct has_number *)self->state;
+    arg  = (struct has_number *)((struct has_obj *)vector_get (args, 0))->state;
+
+    if (this->val > arg->val) {
+        return HAS_TRUE;
+    }
+
+    return HAS_FALSE;
+}
+
+static struct has_obj * _greater_or_equal (struct vm_state * vm, struct has_obj * self, struct vector_state * args) {
+    struct has_number * this;
+    struct has_number * arg;
+
+    this = (struct has_number *)self->state;
+    arg  = (struct has_number *)((struct has_obj *)vector_get (args, 0))->state;
+
+    if (this->val >= arg->val) {
+        return HAS_TRUE;
+    }
+
+    return HAS_FALSE;
+}
+
+static struct has_obj * _lesser (struct vm_state * vm, struct has_obj * self, struct vector_state * args) {
+    struct has_number * this;
+    struct has_number * arg;
+
+    this = (struct has_number *)self->state;
+    arg  = (struct has_number *)((struct has_obj *)vector_get (args, 0))->state;
+
+    if (this->val < arg->val) {
+        return HAS_TRUE;
+    }
+
+    return HAS_FALSE;
+}
+
+static struct has_obj * _lesser_or_equal (struct vm_state * vm, struct has_obj * self, struct vector_state * args) {
+    struct has_number * this;
+    struct has_number * arg;
+
+    this = (struct has_number *)self->state;
+    arg  = (struct has_number *)((struct has_obj *)vector_get (args, 0))->state;
+
+    if (this->val <= arg->val) {
+        return HAS_TRUE;
+    }
+
+    return HAS_FALSE;
+}
+
+static struct has_obj * _logical_and (struct vm_state * vm, struct has_obj * self, struct vector_state * args) {
+    struct has_obj    * arg;
+
+    arg  = (struct has_obj *)vector_get (args, 0);
+
+    if (self != HAS_FALSE && arg != HAS_FALSE) {
+        return HAS_TRUE;
+    }
+
+    return HAS_FALSE;
+}
+
+static struct has_obj * _logical_or (struct vm_state * vm, struct has_obj * self, struct vector_state * args) {
+    struct has_obj    * arg;
+
+    arg  = (struct has_obj *)vector_get (args, 0);
+
+    if (self != HAS_FALSE || arg != HAS_FALSE) {
+        return HAS_TRUE;
+    }
+
     return HAS_FALSE;
 }
 
