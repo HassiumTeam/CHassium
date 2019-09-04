ef0ceb5195b28a98282e5237c5b4d987860872c2
Jacob Misirian <misirianrule@gmail.com>
2019-09-04 13:18:08 -0500

    Implemented mod and mul

--- src/has_lib/modules/default/number.c
+++ src/has_lib/modules/default/number.c
@@ -11,6 +11,8 @@ static struct has_obj * _lesser            (struct vm_state * vm, struct has_obj
 static struct has_obj * _lesser_or_equal   (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
 static struct has_obj * _logical_and       (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
 static struct has_obj * _logical_or        (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
+static struct has_obj * _mod               (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
+static struct has_obj * _mul               (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
 static struct has_obj * to_number          (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
 static struct has_obj * to_string          (struct vm_state * vm, struct has_obj * self, struct vector_state * args);
 
@@ -33,6 +35,8 @@ struct has_obj * has_number_init (float f) {
     has_obj_set_attrib (obj, "_lesser_or_equal",  has_method_init (obj, _greater,     NULL));
     has_obj_set_attrib (obj, "_logical_and",      has_method_init (obj, _logical_and, NULL));
     has_obj_set_attrib (obj, "_logical_or",       has_method_init (obj, _logical_or,  NULL));
+    has_obj_set_attrib (obj, "_mod",              has_method_init (obj, _mod,         NULL));
+    has_obj_set_attrib (obj, "_mul",              has_method_init (obj, _mul,         NULL));
     has_obj_set_attrib (obj, "toNumber",          has_method_init (obj, to_number,    NULL));
     has_obj_set_attrib (obj, "toString",          has_method_init (obj, to_string,    NULL));
 
@@ -69,7 +73,7 @@ static struct has_obj * _bitwise_and (struct vm_state * vm, struct has_obj * sel
 
     num   = (struct has_number *)self->state;
     right = (struct has_number *)((struct has_obj *)vector_get (args, 0))->state;
-    
+
     return has_number_init ((int)num->val & (int)right->val);
 }
 
@@ -185,6 +189,26 @@ static struct has_obj * _logical_or (struct vm_state * vm, struct has_obj * self
     }
 
     return HAS_FALSE;
+}
+
+static struct has_obj * _mod (struct vm_state * vm, struct has_obj * self, struct vector_state * args) {
+    struct has_number * this;
+    struct has_number * right;
+
+    this  = (struct has_number *)self->state;
+    right = (struct has_number *)((struct has_obj *)vector_get (args, 0))->state;
+
+    return has_number_init ((int)this->val % (int)right->val);
+}
+
+static struct has_obj * _mul (struct vm_state * vm, struct has_obj * self, struct vector_state * args) {
+    struct has_number * this;
+    struct has_number * right;
+
+    this  = (struct has_number *)self->state;
+    right = (struct has_number *)((struct has_obj *)vector_get (args, 0))->state;
+
+    return has_number_init (this->val * right->val);
 }
 
 static struct has_obj * to_number (struct vm_state * vm, struct has_obj * self, struct vector_state * args) {
