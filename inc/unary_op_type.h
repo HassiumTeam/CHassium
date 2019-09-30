f021dfad64d4d87e5a8c8324fd416c93e02dbe45
Reagan <misirianrule@gmail.com>
2019-09-30 16:28:44 -0500

    Restarting

--- inc/unary_op_type.h
+++
@@ -1,8 +0,0 @@
-#ifndef _UNARY_OP_TYPE_H_
-#define _UNARY_OP_TYPE_H_
-
-typedef enum {
-    logical_not_unary_op, post_dec_unary_op, post_inc_unary_op, pre_dec_unary_op, pre_inc_unary_op,
-} unary_op_type_t;
-
-#endif
