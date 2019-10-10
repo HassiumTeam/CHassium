adf34aeb6aac943b7eea8706e0057ed329ae2a46
Jacob Misirian <misirianrule@gmail.com>
2019-10-10 17:33:28 -0500

    Set up inst_init

--- src/emit/inst.c
+++ src/emit/inst.c
@@ -40,3 +40,37 @@ void free_inst (struct inst * inst) {
 
     free (inst);
 }
+
+struct inst * bin_op_inst_init (bin_op_type_t type) {}
+struct inst * build_closure_inst_init (void * func) {}
+struct inst * build_exception_handler_inst_init (void * func) {}
+struct inst * call_inst_init (int arg_count) {}
+struct inst * compile_module_inst_init (char * path) {}
+struct inst * import_inst_init (char * file, struct vector * chain) {}
+struct inst * iter_inst_init () {}
+struct inst * iter_full_inst_init () {}
+struct inst * iter_next_inst_init () {}
+struct inst * iter_free_inst_init () {}
+struct inst * jump_inst_init (int label) {}
+struct inst * jump_if_false_inst_init (int label) {}
+struct inst * jump_if_true_inst_init (int label) {}
+struct inst * list_decl_inst_init (int count) {}
+struct inst * load_attrib_inst_init (char * attrib) {}
+struct inst * load_global_inst_init (int symbol) {}
+struct inst * load_id_inst_init (char * id) {}
+struct inst * load_local_inst_init (int symbol) {}
+struct inst * load_number_inst_init (float f) {}
+struct inst * load_string_inst_init (char * str) {}
+struct inst * load_subscript_inst_init () {}
+struct inst * obj_decl_inst_init (struct vector * ids) {}
+//struct inst * obj_destructure_global_inst (struct vector * ids, struct int_vector * indices);
+//struct inst * obj_destructure_local_inst (struct vector * ids, struct int_vector * indices);
+struct inst * pop_inst_init () {}
+struct inst * pop_exception_handler_inst_init () {}
+struct inst * store_attrib_inst_init (char * attrib) {}
+struct inst * store_global_inst_init (int symbol) {}
+struct inst * store_local_inst_init (int symbol) {}
+struct inst * store_subscript_inst_init () {}
+struct inst * super_inst_init (int arg_count) {}
+struct inst * typeof_inst_init () {}
+struct inst * unary_op_inst_init (unary_op_type_t type) {}
